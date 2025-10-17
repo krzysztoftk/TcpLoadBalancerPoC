2a9112fde4c12c2d50184731fe15c38d2c4628f3
Krzysztof Kreis <krzysztof.kreis@mhr.co.uk>
2025-10-17 09:40:17 +0100

    wip

--- TcpCommon.Tests/Client/ClientTests.cs
+++ TcpCommon.Tests/Client/ClientTests.cs
@@ -5,9 +5,10 @@ internal class ClientTests
     private TcpCommon.Client.Client _client;
 
     [Test]
+    [Ignore("until is implemented")]
     public void Test1()
     {
-        
+
         _client.Stop();
     }
 }
